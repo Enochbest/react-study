9f976758b590b09347c5643bbc554b7b35a2730f
Enochbest <2658929241@qq.com>
2024-09-05 23:08:06 +0800

    批量渲染

---
+++ src/10.批量渲染.jsx
@@ -0,0 +1,16 @@
+function App() {
+    const list = ['aaa', 'bbb', 'ccc']
+    for (let i = 0; i < list.length; i++) {
+        list[i] = <li>{list[i]}</li>
+    }
+    return (
+        <div>
+            Hello App
+            <ul>
+                {list}
+            </ul>
+        </div>
+    );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 // import App from './App.jsx'
 // import './index.css';
 // import App from './01.了解JSX.jsx';
-import App from './09.条件渲染.jsx';
+import App from './10.批量渲染.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
