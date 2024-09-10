d8212a1def3aafc544e7a24120cf491ff182208c
Enochbest <2658929241@qq.com>
2024-09-10 19:47:59 +0800

    批量渲染

--- src/10.批量渲染.jsx
+++ src/10.批量渲染.jsx
@@ -1,16 +1,15 @@
 function App() {
-    const list = ['aaa', 'bbb', 'ccc']
-    for (let i = 0; i < list.length; i++) {
-        list[i] = <li>{list[i]}</li>
-    }
-    return (
-        <div>
-            Hello App
-            <ul>
-                {list}
-            </ul>
-        </div>
-    );
+  const list = ['aaa', 'bbb', 'ccc'];
+  for (let i = 0; i < list.length; i++) {
+    list[i] = <li>{list[i]}</li>;
+  }
+  return (
+    <div>
+      Hello App
+      <ul>{list}</ul>
+      <div></div>
+    </div>
+  );
 }
 
 export default App;
